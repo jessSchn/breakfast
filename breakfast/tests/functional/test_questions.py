451d0e05a21cbe74541cebd2ac31964b3163ed01
Alex Brandt <alunduil@alunduil.com>
2011-10-10 15:43:24 -0500

    Adding the questions controller.

---
+++ breakfast/tests/functional/test_questions.py
@@ -0,0 +1,7 @@
+from breakfast.tests import *
+
+class TestQuestionsController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='questions', action='index'))
+        # Test response...
