af8823a6f167806bdd9d855abc4aa25b5c1b6714
Alex Brandt <alunduil@alunduil.com>
2011-10-10 15:44:09 -0500

    Removing the frontend controller.

--- breakfast/tests/functional/test_frontend.py
+++
@@ -1,7 +0,0 @@
-from breakfast.tests import *
-
-class TestFrontendController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='frontend', action='index'))
-        # Test response...
